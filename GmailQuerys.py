f430719aaa57a703a9ed7be9e80aeba7eacaa2c3
youkusa <wyhwow1995@gmail.com>
2021-11-25 14:33:35 +0000

    use new google mail api

--- GmailQuerys.py
+++ GmailQuerys.py
@@ -6,6 +6,7 @@ import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 from googleapiclient import errors
 import base64
 import os
